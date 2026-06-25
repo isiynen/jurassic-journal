20187c27f350e4db938159d9a9d689779800495b
isiynen <isiynen@yahoo.com>
2026-06-24 22:53:48 -0400

    Build: sign debug builds with release key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("release")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
