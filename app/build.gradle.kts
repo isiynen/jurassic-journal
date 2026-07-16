8ce6dd6ae694d32d5ae3d9f40c2953ef7d376271
isiynen <isiynen@yahoo.com>
2026-07-15 21:02:10 -0400

    Bump app version to 1.7.5 (versionCode 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.sufficienteffort.jurassicjournal"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.7.4"
+        versionCode = 14
+        versionName = "1.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
