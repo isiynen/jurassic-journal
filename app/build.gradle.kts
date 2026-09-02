d977c3f29ece849729d0013d34ebdfac1002f9b9
isiynen <isiynen@yahoo.com>
2026-09-02 12:47:00 -0400

    Fixed several dinos not showing enhancements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.sufficienteffort.jurassicjournal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.7.15"
+        versionCode = 27
+        versionName = "1.7.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
